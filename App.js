65835057a8ef72025a29fa5cae17e6f58c2bbbaa
YOLO30117 <yologamex30117@gmail.com>
2021-09-15 21:17:42 +0530

    monkeyChunky part 1

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View ,TextInput , TouchableOpacity} from 'react-native';
+import { Header } from 'react-native-elements'
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      text : '',
+      display : '',
+    }
+  }
+  render() {
+    return (
+      <SafeAreaProvider>
+      <View style={styles.container}>
+        <Header
+        backgroundColor = {"purple"}
+        leftComponent={{ icon: 'menu', color: '#fff', iconStyle: { color: '#fff' } }}
+        centerComponent={{ text: 'Monkey Chunky', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+      />
+      <TextInput
+        onChangeText = {(info)=>{
+          this.setState({
+            text : info,
+          })
+        }}
+        value = {this.state.text}
+        style = {styles.inputBox} 
+      />
+      <TouchableOpacity 
+      style = {styles.goButton}
+      onPress = {()=>{
+        this.setState({
+          display : this.state.text
+        })
+      }}>
+        <Text style = {styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
+      <Text style = {styles.displayText}>
+        {this.state.display}
+      </Text>
+      </View>
+      </SafeAreaProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#b8b8b8',
+  },
+  inputBox: {
+    marginTop: 200,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    outline: 'none',
+  },
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  displayText: {
+    textAlign: 'center',
+    fontSize: 30,
   },
 });
